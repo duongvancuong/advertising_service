a532597e5901a412fd97895292b5f828cc96605d
duongvancuong <vancuong279dn@gmail.com>
2018-06-23 13:56:51 +0700

    Apply redux-saga

---
+++ src/store/configureStore.js
@@ -0,0 +1,37 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+import { subscribeStore, loadState } from '../services/localstorage';
+// Returns the store instance
+// It can  also take initialState argument when provided
+
+const persistedState = loadState();
+
+const configureStoreDev = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = {
+    ...createStore(
+      rootReducer,
+      persistedState,
+      composeEnhancers(applyMiddleware(sagaMiddleware)),
+    ),
+    runSaga: sagaMiddleware.run(rootSaga),
+  };
+  subscribeStore(store);
+  return store;
+};
+
+const configureStoreProd = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  return {
+    ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
+    runSaga: sagaMiddleware.run(rootSaga)
+  };
+};
+
+const env = process.env.NODE_ENV;
+const configureStore = env === 'development' ? configureStoreDev : configureStoreProd;
+
+export default configureStore;
