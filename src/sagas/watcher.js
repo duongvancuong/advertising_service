a709ecb414ee4436418aac520c37057cb5018346
duongvancuong <vancuong@279dn@gmail.com>
2018-06-25 16:23:22 +0700

    Add more structor

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,5 +1,7 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeEvery, race, take, put } from 'redux-saga/effects';
+
 import * as types from '../constants/actionTypes';
+import {} from '../actions'; /* Import actions saga. */
 
 // Watches for SEARCH_MEDIA_REQUEST action type asynchronously
 export default function* root() {
